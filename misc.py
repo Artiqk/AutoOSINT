6d9cf63484b8d0118cd0a298c04ca0a0782f504d
antoine <antoineabonneau94@gmail.com>
2022-11-24 10:48:59 -0500

    Added dnscan function

--- misc.py
+++ misc.py
@@ -1,9 +1,24 @@
+import os
+
+dnscan_path = "./dnscan/dnscan.py"
+
+def dnscan(domain):
+    dir_path = "scans/" + domain + "/dnscan/"
+    if '@' in domain:
+        domain = domain.split('@')[1]
+    file_name = "result.txt"
+    cmd_dir = "mkdir -p " + dir_path
+    cmd_scan = dnscan_path + " -d " + domain + " -r -o " + dir_path + file_name
+    os.system(cmd_dir)
+    os.system(cmd_scan)
+
+
 def handle_domain(domain):
-    print(domain)
+    dnscan(domain)
 
 
 def handle_mail(mail):
-    print(mail)
+    dnscan(mail)
 
 
 def handle_argument(arg, callback):
