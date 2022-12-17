1dc3f6574fe7f4557a36a085cd9b6d9f303489d3
antoine <antoineabonneau94@gmail.com>
2022-12-17 17:51:37 -0500

    added minor

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install theharvester -y
-python3 -m pip install pyyaml
+python3 -m pip install pyyaml json shodan
 git clone https://github.com/rbsec/dnscan.git
 python3 -m pip install -r dnscan/requirements.txt
