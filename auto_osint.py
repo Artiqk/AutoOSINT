89af975b57378f49a9fdd03f81286f5fb0ded771
antoine <antoineabonneau94@gmail.com>
2022-12-17 17:31:21 -0500

    Removed mail - useless

--- auto_osint.py
+++ auto_osint.py
@@ -7,12 +7,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain", help="target single ip/domain")
 parser.add_argument("-D", "--domains", help="target multiple ip/domain from file")
 
-parser.add_argument("-m", "--mail", help="target single mail")
-parser.add_argument("-M", "--mails", help="target multiple mails from file")
-
 args = parser.parse_args()
 
 handle_argument(args.domain, handle_domain)
-# handle_argument(args.mail, handle_mail)
+handle_argument(args.domains, handle_domains)
 
-print(f"Scans results are stored under : {os.path.abspath(f'scans/{args.domain}/')}")
+if args.domain:
+    print(f"[*] Scans results are stored under : {os.path.abspath(f'scans/{args.domain}/')}")
